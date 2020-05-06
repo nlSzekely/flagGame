38323580cffc06de81d3d55ad1eb1c52a94575f4
nlSzekely <32170516+nlSzekely@users.noreply.github.com>
2020-05-06 09:20:15 +0300

    Add files via upload

---
+++ precache-manifest.69bad92945e7deeaac1e15a67d28d1a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33992648030e5e48e696569df52bc90d",
+    "url": "/flagGame/index.html"
+  },
+  {
+    "revision": "4c3a08143efcdb7466b6",
+    "url": "/flagGame/static/css/main.99764911.chunk.css"
+  },
+  {
+    "revision": "9bbbf50607f1747dfe8f",
+    "url": "/flagGame/static/js/2.b4718d82.chunk.js"
+  },
+  {
+    "revision": "4ae2e24854201e2c90f766e681fee893",
+    "url": "/flagGame/static/js/2.b4718d82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c3a08143efcdb7466b6",
+    "url": "/flagGame/static/js/main.f6cffe86.chunk.js"
+  },
+  {
+    "revision": "8c8132b35c2a54e4b078",
+    "url": "/flagGame/static/js/runtime-main.4a7a50a8.js"
+  }
+]);
